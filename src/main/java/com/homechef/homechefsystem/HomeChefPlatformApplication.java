2ab1d58029fb741b6841e3c49699375d98edcc2b
wangqi <1325041344@qq.com>
2026-03-16 22:31:26 +0800

    初始化

---
+++ src/main/java/com/homechef/homechefsystem/HomeChefPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.homechef.homechefsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeChefPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeChefPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homechef/homechefsystem/HomeChefPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homechef.homechefsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeChefPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
